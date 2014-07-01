a1317620c5755d37f6e9f6281f2d0e8be045e2d6
Alexander <alexandermichaelmiranda@gmail.com>
2014-06-30 22:08:42 -0700

    Wrapped map initialization into document.ready

--- js/map.js
+++ js/map.js
@@ -1,25 +1,24 @@
-var osm = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+$(document).ready(function() {
+  var url = 'http://data.seattle.gov/resource/7ckr-2zz9.json';
+  var osm = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://stamen.com">Stamen Design</a>' 
     });
 
-var map = L.map('map', {
-	center: [47.624215, -122.334931],
-	zoom: 12,
-	layers: [osm]
-});
+  var map = L.map('map', {
+    center: [47.624215, -122.334931],
+    zoom: 12,
+    layers: [osm]
+  });
 
-L.control.locate({
-  locateOptions: {
-    maxZoom: 16
-  },
-  follow: true
-}).addTo(map);
+  L.control.locate({
+    locateOptions: {
+      maxZoom: 16
+    },
+    follow: true
+  }).addTo(map);
 
-L.control.scale().addTo(map);
+  L.control.scale().addTo(map);
 
-var url = 'http://data.seattle.gov/resource/7ckr-2zz9.json';
-
-$(document).ready(function() {
   $.getJSON(url, function(data, textStatus) {
     $.each(data, function(i, entry) {
       L.marker([entry.latitude, entry.longitude]).addTo(map)
